91f21b04580c52dc7006d382882bdaeb62521a3d
Polina Miftakhudinova <rambeeech@gmail.com>
2021-07-12 20:45:49 +0400

    Tag generation(step 2)

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,19 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Tag
+    def self.build(tag, hash = {})
+      line = "<#{tag}"
+      hash.each do |key, value|
+        line += " #{key}=\"#{value}\""
+      end
+      line += ">"
+      if block_given?
+        line += "#{yield}</#{tag}>"
+      else
+        line
+      end
+    end
+  end
 end
