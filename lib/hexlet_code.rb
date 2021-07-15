f6de6862ebfb52791ca320be74755356ac175fee
Polina Miftakhudinova <rambeeech@gmail.com>
2021-07-15 16:24:05 +0400

    Class for generation of specific fields(step 5)

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -11,20 +11,55 @@ module HexletCode
     def self.build(tag, hash = {})
       line = "<#{tag}"
       hash.each do |key, value|
-        line += " #{key}=\"#{value}\""
+        line += " #{key}=\"#{value}\"" if key != :selected
       end
-      line += ">"
-      if block_given?
-        line += "#{yield}</#{tag}>"
-      else
-        line
-      end
+      line += " selected" if hash[:selected]
+      line += if block_given?
+                ">#{yield}</#{tag}>"
+              else
+                ">"
+              end
     end
   end
 
-  def self.form_for(user, url: '#', &block)
-    form = Tag.build("form", action: url, method: "post") do
-      "\n"
+  # This class generates specific fields of form
+  class Form
+    attr_reader :user
+
+    def initialize(user)
+      @user = user
+      @fields = ""
+    end
+
+    def select_constructor(field_name, collection)
+      Tag.build("select", name: field_name) do
+        options = collection.each_with_object([]) do |element, object|
+          object << if user.gender == element
+                      Tag.build("option", value: element, selected: true) { element }
+                    else
+                      Tag.build("option", value: element) { element }
+                    end
+        end
+        options.join
+      end
+    end
+
+    def input(field_name, as: nil, collection: [])
+      @fields += case as
+                 when :text
+                   Tag.build("textarea", cols: 20, rows: 40, name: field_name) { user.send(field_name) }
+                 when :select
+                   select_constructor(field_name, collection)
+                 else
+                   Tag.build("input", type: "text", value: user.send(field_name), name: field_name)
+                 end
+    end
+  end
+
+  def self.form_for(user, url: "#")
+    form = Form.new(user)
+    Tag.build("form", action: url, method: "post") do
+      yield form
     end
   end
 end
---
+++ test/hexlet_code_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class HexletCodeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::HexletCode::VERSION
+  end
+
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
+
+  def test_builder
+    assert_equal "<br>", HexletCode::Tag.build("br")
+    assert_equal "<img src=\"path/to/image\">", HexletCode::Tag.build("img", src: "path/to/image")
+    assert_equal "<input type=\"submit\" value=\"Save\">", HexletCode::Tag.build("input", type: "submit", value: "Save")
+    assert_equal "<label>Email</label>", HexletCode::Tag.build("label") { "Email" }
+    assert_equal "<label for=\"email\">Email</label>", HexletCode::Tag.build("label", for: "email") { "Email" }
+  end
+
+  def test_selected
+    assert_equal "<option value=\"m\" selected>m</option>", HexletCode::Tag
+      .build("option", value: "m", selected: true) { "m" }
+  end
+
+  def test_empty_form_for
+    user = User.new name: "rob"
+    form = HexletCode.form_for user do |f|
+    end
+    assert_equal "<form action=\"#\" method=\"post\"></form>", form
+    form = HexletCode.form_for user, url: "/users" do |f|
+    end
+    assert_equal "<form action=\"/users\" method=\"post\"></form>", form
+  end
+
+  def test_form_for_with_fields
+    user = User.new name: "rob", job: "hexlet", gender: "m"
+    form = HexletCode.form_for user do |f|
+      f.input :name
+      f.input :job, as: :text
+      f.input :gender, as: :select, collection: %w[m f]
+    end
+    should_return = "<form action=\"#\" method=\"post\"><input type=\"text\" value=\"rob\" name=\"name\">" \
+    "<textarea cols=\"20\" rows=\"40\" name=\"job\">hexlet</textarea><select name=\"gender\">" \
+    "<option value=\"m\" selected>m</option><option value=\"f\">f</option></select></form>"
+    assert_equal should_return, form
+  end
+end
