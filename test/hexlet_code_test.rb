a0bf804b8e5e00c9e4ea1b32e4676a8f73de28bc
Polina Miftakhudinova <rambeeech@gmail.com>
2021-07-16 15:46:27 +0400

    Update

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -22,29 +22,26 @@ class HexletCodeTest < Minitest::Test
       .build('option', value: 'm', selected: true) { 'm' }
   end
 
-  def test_form_for_with_fields
+  def test_form_for
     user = User.new name: 'rob', job: 'hexlet', gender: 'm'
     form = HexletCode.form_for user do |f|
-      f.input :name
       f.input :job, as: :text
       f.input :gender, as: :select, collection: %w[m f]
     end
-    assert_equal form, '<form action="#" method="post"><label for="name">Name</label>' \
-                       '<input name="name" type="text" value="rob"><label for="job">Job</label>' \
+    assert_equal form, '<form action="#" method="post"><label for="job">Job</label>' \
                        '<textarea name="job" cols="20" rows="40">hexlet</textarea><label for="gender">Gender</label>' \
-                       '<select name="gender"><option value="m" selected>m</option><option value="f">f</option></select></form>'
+                       '<select name="gender"><option value="m" selected>m</option><option value="f">f</option>' \
+                       '</select></form>'
   end
 
   def test_submit
     user = User.new job: 'hexlet'
     form = HexletCode.form_for user do |f|
       f.input :name
-      f.input :job
       f.submit
     end
     should_return = '<form action="#" method="post"><label for="name">Name</label>' \
-                    '<input name="name" type="text" value=""><label for="job">Job</label>' \
-                    '<input name="job" type="text" value="hexlet"><input type="submit" value="Save" name="commit"></form>'
+                    '<input name="name" type="text" value=""><input type="submit" value="Save" name="commit"></form>'
     assert_equal should_return, form
   end
 end
