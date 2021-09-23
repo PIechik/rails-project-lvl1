96c5787a1bb3e33d82708b6a4038f3c03bc385a7
Polina Miftakhudinova <rambeeech@gmail.com>
2021-09-23 16:08:16 +0400

    Fixing failing test

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,6 +6,7 @@ module HexletCode
   autoload :Inputs, 'hexlet_code/inputs.rb'
   autoload :Label, 'hexlet_code/label.rb'
   autoload :Tag, 'hexlet_code/tag.rb'
+  autoload :VERSION, 'hexlet_code/version.rb'
 
   def self.form_for(entity, hash = {})
     form = Form.new(hash)
